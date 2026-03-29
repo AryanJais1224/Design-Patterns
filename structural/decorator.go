3340c6c3f82adce6e37c04d2d6e2e98adcab4f4c
Aryann <129775380+AryanJais1224@users.noreply.github.com>
2026-03-29 22:41:35 +0530

    Create decorator.go

---
+++ structural/decorator.go
@@ -0,0 +1,6 @@
+func Logger(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        log.Println("Request received")
+        next.ServeHTTP(w, r)
+    })
+}
